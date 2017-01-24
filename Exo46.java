b11fbb64a96b43a4116edcdff0c6395204a36eb1
agboli16 <agboli16@b042-29.crio.dauphine.fr>
2017-01-24 19:23:24 +0100

    Exo36

---
+++ Exo46.java
@@ -0,0 +1,64 @@
+/**
+ * This program finds an integer between 1 and 10000 that has
+ * the largest number of divisors.  It prints out the maximum
+ * number of divisors and an integer that has that many divisors.
+ */
+
+public class Exo46 {
+
+   public static void main(String[] args) {
+   
+       int N;            // One of the integers whose divisors we have to count.
+       int maxDivisors;  // Maximum number of divisors seen so far.
+       int numWithMax;   // A value of N that had the given number of divisors.
+       int sauvegarde[]= new int[10001];
+       
+       maxDivisors = 1;  // Start with the fact that 1 has 1 divisor.
+       numWithMax = 1;
+
+       /* Process all the remaining values of N from 2 to 10000, and
+          update the values of maxDivisors and numWithMax whenever we
+          find a value of N that has more divisors than the current value
+          of maxDivisors.
+       */
+       
+       for ( N = 2;  N <= 10000;  N++ ) {
+       
+           int D;  // A number to be tested to see if it's a divisor of N.
+           int divisorCount;  // Number of divisors of N.
+           
+          
+           
+           divisorCount = 0;
+           sauvegarde[1] = 1;
+           
+           for ( D = 1;  D <= N;  D++ ) {  // Count the divisors of N.
+              if ( N % D == 0 )
+                 divisorCount++;
+
+           }
+              sauvegarde[N]=divisorCount;   
+              
+              
+           if (divisorCount > maxDivisors) {
+              maxDivisors = divisorCount;
+              numWithMax = N;
+           }
+       
+       }
+       
+       System.out.println("Among integers between 1 and 10000,");
+       System.out.println("The maximum number of divisors is " + maxDivisors);
+       System.out.println("A number with " + maxDivisors + " divisors include ");
+       for (N = 1; N <=10000; N++) {
+    	   if (sauvegarde[N] == maxDivisors){
+    		   System.out.println(N);
+    		   
+    		   
+    	   }
+       }
+   
+   } // end main()
+
+}
+
